fce4e779ac67b2bd2e67303a872f91ab7d56362d
Luka-DV <luka.guva@gmail.com>
2024-06-17 19:00:42 +0200

    finish seq anim tasks

--- MDN/sequencing_animations/start/main.js
+++ MDN/sequencing_animations/start/main.js
@@ -12,3 +12,70 @@ const aliceTiming = {
 const alice1 = document.querySelector("#alice1");
 const alice2 = document.querySelector("#alice2");
 const alice3 = document.querySelector("#alice3");
+
+
+// 1. "callback hell":
+
+/* 
+
+alice1.animate(aliceTumbling, aliceTiming).finished
+.then( () => {
+  const animationFinished = alice2.animate(aliceTumbling, aliceTiming).finished;
+  animationFinished.then( () => {
+      alice3.animate(aliceTumbling, aliceTiming);
+  });
+});
+
+ */
+
+//2. promise chaining:
+
+/* 
+alice1.animate(aliceTumbling, aliceTiming).finished
+.then( () => {
+  return alice2.animate(aliceTumbling, aliceTiming).finished;
+})
+.then( () => {
+  return alice3.animate(aliceTumbling, aliceTiming).finished;
+})
+.catch(error => {
+  console.error(`Error in animation: ${error}`)
+});
+
+ */
+//or:
+
+/* 
+alice1.animate(aliceTumbling, aliceTiming).finished
+.then(() => alice2.animate(aliceTumbling, aliceTiming).finished)
+.then(() => alice3.animate(aliceTumbling, aliceTiming).finished)
+.catch(error => console.error(`Error in animation: ${error}`));
+
+ */
+// 3. asnyc/await form:
+
+/* 
+async function animateAlices() {
+  try {
+    const firstAnimFinish = await alice1.animate(aliceTumbling, aliceTiming).finished;
+    const secondAnimFinish = await alice2.animate(aliceTumbling, aliceTiming).finished;
+    const thirdAnimFinish = await alice3.animate(aliceTumbling, aliceTiming).finished;
+  } 
+  catch (error) {
+    console.error(`Error in animation: ${error}`)
+  }
+}
+ */
+
+async function animateAlices() {
+  try {
+    await alice1.animate(aliceTumbling, aliceTiming).finished;
+    await alice2.animate(aliceTumbling, aliceTiming).finished;
+    await alice3.animate(aliceTumbling, aliceTiming).finished;
+  } 
+  catch (error) {
+    console.error(`Error in animation: ${error}`)
+  }
+}
+
+animateAlices();
