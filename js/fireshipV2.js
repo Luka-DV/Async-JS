4875bcad4d2820f7dbba264258221a151946627c
Luka-DV <luka.guva@gmail.com>
2024-06-18 12:53:06 +0200

    add code for testing performance

--- js/fireshipV2.js
+++ js/fireshipV2.js
@@ -2,14 +2,25 @@
 
 
 const getFruit =  async name => {
-    const fruits = {
-        pineapple: "PIn",
-        peach: "PEa",
-        strawberry: "STr"
-    }
+        const fruits = {
+            pineapple: "PIn",
+            peach: "PEa",
+            strawberry: "STr"
+        }
 
-    return fruits[name];
+        //for testing:
+
+        return new Promise( resolve => {
+            setTimeout( () => {
+                resolve(fruits[name]);
+            },500)
+        })
+    
+        //return fruits[name];
 }
+
+
+
 
 /* const makeSmoothie = async () => {
     return Promise.all([getFruit("pineapple"), getFruit("peach")])
@@ -20,7 +31,7 @@ const getFruit =  async name => {
 makeSmoothie(); */
 
 // or:
-
+/* 
 const makeSmoothie = async () => {
     const a = getFruit("pineapple");
     const b = getFruit("peach");
@@ -34,3 +45,30 @@ const makeSmoothie = async () => {
 makeSmoothie()
     .then(data => console.log(data))
     .catch(error => console.error(error));
+ */
+
+const fruits = ["pineapple", "peach", "strawberry"];
+
+const fruitLoop1 = async () => {
+    for(const fruit of fruits) {
+        const emoji = await getFruit(fruit);  //getFruit is an async function that returns a promise
+        const d = new Date();
+        console.log(emoji, `${d.getSeconds()},${d.getMilliseconds()}`);
+    }
+}
+
+//fruitLoop1();
+
+
+const smoothie = fruits.map(f => getFruit(f));
+console.log(smoothie) 
+
+const fruitLoop2 = async () => {
+    for await (const emoji of smoothie) { //waits for all the items to resolve and then loops immediately after
+        const d = new Date();
+        console.log(emoji, `${d.getSeconds()},${d.getMilliseconds()}`);
+    }
+}
+
+fruitLoop2();
+
